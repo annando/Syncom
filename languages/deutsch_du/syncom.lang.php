39d704c33ba2e8744f36da105e81587d41af3392
Michael Vogel <icarus@dabo.de>
2011-06-12 13:39:10 +0000

    - README aktualisiert - Sprachdateien hinzugefügt

---
+++ languages/deutsch_du/syncom.lang.php
@@ -0,0 +1,4 @@
+<?php
+$l['syncom_realname'] = "Name für den Sync:";
+$l['syncom_realmail'] = "Mailadresse für den Sync:";
+?>
---
+++ languages/english/syncom.lang.php
@@ -0,0 +1,4 @@
+<?php
+$l['syncom_realname'] = "Name for the sync:";
+$l['syncom_realmail'] = "Mail address for the sync:";
+?>
