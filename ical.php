74f4b143d5cb580c490080122a65de1b13707572
Michael Vogel <icarus@dabo.de>
2011-09-27 12:35:40 +0000

    Code aufgeraeumt, To-Do deutlich gemacht

--- ical.php
+++ ical.php
@@ -29,6 +29,9 @@ header('Expires: Sun, 19 Nov 1978 05:00:00 GMT');
 header('Content-Disposition: attachment; filename="calendar.ics";');
 header("Content-Type: text/calendar; charset=utf-8");
 
+// Zeitzone auf UTC stellen, damit die Kalenderdaten nicht umgerechnet werden
+date_default_timezone_set("UTC");
+
 echo "BEGIN:VCALENDAR\r\n";
 echo "VERSION:2.0\r\n";
 echo "PRODID:https://news.piratenpartei.de/calendar.php\r\n";
@@ -37,7 +40,7 @@ echo "METHOD:PUBLISH\r\n";
 echo "X-WR-CALNAME: Piratenkalender\r\n";
 
 // To-Do: Sinnvoller Datumsbereich, Kalender ueber Parameter
-$events = get_events(2, 1, 11111111111, false);
+$events = get_events(1, 1, 11111111111, false);
 // $events_cache = get_events($calendar['cid'], $start_timestamp, $end_timestamp, $calendar_permissions['canmoderateevents']);
 
 foreach ($events as $day=>$events2) {
@@ -49,53 +52,48 @@ foreach ($events as $day=>$events2) {
 	$nextday = $dateday + 86500;
 	$strnext = date("Ymd", $nextday);
 
-   foreach ($events2 as $event) {
-	$line = "BEGIN:VEVENT\r\n";
-	// $line .= "CATEGORIES:Stammtisch\r\n";
-	$line .= "CLASS:PUBLIC\n\r";
-	$line .= "UID:".$day."-".$event['eid']."-".$event['cid']."-".$event['uid']."\r\n";
-	// ORGANIZER;CN="Berlin Landesverband":MAILTO:kalender@piratenpartei-bayern.de
-	$line .= "SUMMARY:".addcslashes(trim($event['name']), ",;\n".'')."\r\n";
+	foreach ($events2 as $event) {
+		$line = "BEGIN:VEVENT\r\n";
+		$line .= "CLASS:PUBLIC\n\r";
+		$line .= "UID:".$day."-".$event['eid']."-".$event['cid']."-".$event['uid']."\r\n";
+		$line .= "SUMMARY:".addcslashes(trim($event['name']), ",;\n".'')."\r\n";
 
-	$parser = new postParser;
+		$parser = new postParser;
 
-	$parser_options = array(
-			'allow_html' => 0,
-			'filter_badwords' => 1,
-			'allow_mycode' => 1,
-			'allow_smilies' => 0,
-			'allow_imgcode' => 0,
-			"filter_badwords" => 1
-			);
-        $parsed = $parser->parse_message($event['description'], $parser_options);
+		$parser_options = array(
+				'allow_html' => 0,
+				'filter_badwords' => 1,
+				'allow_mycode' => 1,
+				'allow_smilies' => 0,
+				'allow_imgcode' => 0,
+				"filter_badwords" => 1
+				);
+	        $parsed = $parser->parse_message($event['description'], $parser_options);
 
-	$line .= "DESCRIPTION:".addcslashes(trim(str_replace("\n","\n ",html_entity_decode(strip_tags($parsed)))), ",;".'"')."\r\n";
+		// To-Do: Zeilenumbrueche muessen noch uebernommen werden
+		$line .= "DESCRIPTION:".addcslashes(trim(str_replace("\n","\n ",html_entity_decode(strip_tags($parsed)))), ",;".'"')."\r\n";
 
-	//$line .= "LOCATION:Neue Str. 58 Eingang Lämmertwiete\r\n";
-	if ($event['endtime'] == 0) {
-		$line .= "DTSTART;VALUE=DATE:".$strdate."\n\r";
-		$line .= "DTEND;VALUE=DATE:".$strnext."\n\r";
-	} else {
-		$line .= "DTSTART:".icaldate($event['starttime'], $strdate)."\r\n";
-		$line .= "DTEND:".icaldate($event['endtime'], $strdate)."\r\n";
+		// To-Do: Diese Felder fuellen, benotigt aber neue Felder in DB
+		// $line .= "LOCATION:Neue Str. 58 Eingang Lämmertwiete\r\n";
+		// $line .= "CATEGORIES:Stammtisch\r\n";
+
+		if (!$event['usingtime']) {
+			$line .= "DTSTART;VALUE=DATE:".$strdate."\n\r";
+			$line .= "DTEND;VALUE=DATE:".$strnext."\n\r";
+		} else {
+			$line .= "DTSTART:".icaldate($event['starttime'], $strdate)."\r\n";
+			$line .= "DTEND:".icaldate($event['endtime'], $strdate)."\r\n";
+		}
+		$line .= "DTSTAMP:".icaldate($event['dateline'])."\r\n";
+
+		// To-Do: URL muss noch dynamisch sein
+		$line .= "URL;VALUE=URI:https://news.piratenpartei.de/calendar.php?action=event&eid=".$event['eid']."\r\n";
+
+		$line .= "END:VEVENT\r\n";
+		$line = str_replace("\r", "", $line);
+		$line = str_replace("\n", "\r\n", $line);
+		echo $line;
 	}
-	$line .= "DTSTAMP:".icaldate($event['dateline'])."\r\n";
-	$line .= "URL;VALUE=URI:https://news.piratenpartei.de/calendar.php?action=event&eid=".$event['eid']."\r\n";
-
-	//echo "ignoretimezone ".$event['ignoretimezone']."\r\n";
-	//echo "usingtime ".$event['usingtime']."\r\n";
-	//echo "repeats ".$event['repeats']."\r\n";
-	//echo "import_eid ".$event['import_eid']."\r\n";
-	//echo "timezone ".$event['timezone']."\r\n";
-	$line .= "END:VEVENT\r\n";
-	$line = str_replace("\r", "", $line);
-	$line = str_replace("\n", "\r\n", $line);
-	echo $line;
-   }
 }
 echo "END:VCALENDAR\r\n";
-
-// To-Do fuer spaeter:
-// - Zeitzone konfigurieren
-// - Location hinzufügen
 ?>
